f1c94830c5724e7b4577a77d356d1ca0c3425b1c
Nera1072 <nmakudexa001@st.ug.edu.gh>
2021-10-26 21:13:23 +0000

    descriptive comment for code chsnge

---
+++ index.js
@@ -0,0 +1,25 @@
+//alert("I love God");
+//console.log(" i really love  everything");
+//var age = 23;
+ 
+
+//console.log("Your age is "+age)
+//var firstName = "yo";
+//console.log('hello', firstName)
+//var online = true;
+//console.log('are u online:',online)
+//var myName = window.prompt("what is your name");
+//console.log("hello",myName);
+function myName(){
+    var result =( 2+2) + 20;
+    return result;
+    console.log(result)
+};
+myName()
+
+
+
+
+
+
+
